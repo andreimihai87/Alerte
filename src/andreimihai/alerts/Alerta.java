154f891952e55131a32f3497d69d6decf4b658f4
andreimihai87 <andreidocleanu@yahoo.com>
2015-09-12 22:47:29 +0300

    Comit initial

---
+++ src/andreimihai/alerts/Alerta.java
@@ -0,0 +1,11 @@
+package andreimihai.alerts;
+import java.util.Date;
+
+
+public class Alerta {
+
+	String titlu;
+	String alteInfo;
+	Date data = new Date();
+	
+}
---
+++ src/andreimihai/alerts/ClientConsola.java
@@ -0,0 +1,12 @@
+package andreimihai.alerts;
+
+public class ClientConsola {
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
