e540e38e5d33c2039322c02d30c50a22d61ef080
andreimihai87 <andreidocleanu@yahoo.com>
2015-09-13 21:10:16 +0300

    #11

--- src/andreimihai/alerts/Manager.java
+++ src/andreimihai/alerts/Manager.java
@@ -1,17 +1,26 @@
 package andreimihai.alerts;
 
+import java.util.ArrayList;
+
 public class Manager {
 
+	private ArrayList<Category> categoryList = new ArrayList<Category>();
+	private ArrayList<Alert> alertList = new ArrayList<Alert>();
+	
 	public void addCategory(Category cat){
-		
+		if(!categoryList.contains(cat)){
+			categoryList.add(cat);
+		}
 	}
 	
 	public void addAlert(Alert alert){
-		
+		alertList.add(alert);
 	}
 	
 	public void viewCategories(){
-		
+		for(int i=0; i<categoryList.size(); i++){
+			System.out.println(categoryList.get(i).getName());
+		}
 	}
 	public void viewAlerts() {
 		
--- src/andreimihai/alerts/test/ClientConsole.java
+++ src/andreimihai/alerts/test/ClientConsole.java
@@ -1,14 +1,22 @@
 package andreimihai.alerts.test;
 
-import andreimihai.alerts.Alert;
-import andreimihai.alerts.Category;
+import andreimihai.alerts.*;
 
 public class ClientConsole {
 
 	public static void main(String[] args) {
 		
+		Manager allStuff = new Manager();
+		
 		Category oneCategory = new Category("Buletin");
+		
+		allStuff.addCategory(oneCategory);
+		
 		Category secondCategory = new Category("Permis");
+		
+		allStuff.addCategory(secondCategory);
+		//allStuff.addCategory(secondCategory);
+		//am testat sa vad daca se mai adauga inca una
 		
 		System.out.println(oneCategory.getName());
 		System.out.println(secondCategory.getName());
@@ -18,6 +26,8 @@ public class ClientConsole {
 		
 		System.out.println(firstAlert);
 		System.out.println(secondAlert);
+		
+		allStuff.viewCategories();
 
 	}
 
