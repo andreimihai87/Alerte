5853ef490f3d117cc0f943b42cde415bf2d19451
andreimihai87 <andreidocleanu@yahoo.com>
2015-09-14 23:27:40 +0300

    #15 in progress

---
+++ src/andreimihai/alerts/gui/MainWindow2.java
@@ -0,0 +1,19 @@
+package andreimihai.alerts.gui;
+
+import javax.swing.*;
+
+
+public class MainWindow2 extends JFrame {
+
+		public static void main(String[] args) {
+			
+			WindowAddAlert f = new WindowAddAlert("Adauga Alerta");
+			f.setVisible(true);
+			f.setLayout(null);
+			f.setSize(600, 400);
+			
+					
+		}
+	
+
+}
--- src/andreimihai/alerts/gui/WindowAddAlert.java
+++ src/andreimihai/alerts/gui/WindowAddAlert.java
@@ -1,5 +1,53 @@
 package andreimihai.alerts.gui;
 
-public class WindowAddAlert {
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+
+public class WindowAddAlert extends JFrame{
+	
+		JLabel messages;
+		JButton addAlert;
+		
+		public WindowAddAlert(String titlu){
+			super(titlu);
+			this.addWindowListener(new WindowAdapter(){
+				public void windowClosing ( WindowEvent e) {
+					System.exit (0);
+			}
+		});
+		setLayout(null);
+			
+		setBackground(Color.GREEN);
+		
+		messages = new JLabel("");
+		messages.setBounds(10, 120, 120, 20);
+		add(messages);
+		
+		JLabel cat = new JLabel("Categorie");
+		cat.setBounds(10, 10, 100, 20);
+		add(cat);
+				
+		JTextField catText = new JTextField();
+		catText.setBounds(10, 40, 80, 20);
+		add(catText);
+		
+		addAlert = new JButton("Adauga Alerta");
+		addAlert.setBounds(10, 80, 120, 20);
+		add(addAlert);
+		
+		
+		
+		addAlert.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				messages.setText("Alerta adaugata!");
+			}
+		});
+	
+	}
 
 }
