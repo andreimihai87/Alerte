205d72a013b5b9aceba0c2ce3c53589ec5a28ae8
FallenLeaves <dandocleanu@gmail.com>
2015-09-15 15:52:10 +0300

    Added a menu to the main window, Task #16

--- src/andreimihai/alerts/gui/MainWindow.java
+++ src/andreimihai/alerts/gui/MainWindow.java
@@ -5,8 +5,14 @@ package andreimihai.alerts.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 /**
@@ -14,6 +20,11 @@ import javax.swing.SwingUtilities;
  */
 public class MainWindow extends JFrame {
 	
+	private JMenuBar menuBar;
+	private JMenu menu;
+	
+	private JPanel current = null;
+
 	public MainWindow() {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,15 +36,62 @@ public class MainWindow extends JFrame {
 		setMinimumSize(dimension);
 
 		setLayout(new BorderLayout());
+		
+		createMenu();
+		
 		loadSomething();
 		
 		pack();
 	}
 	
-	private void loadSomething() {
-		//add(new WindowAbout(), BorderLayout.CENTER);
-		add(new WindowAddAlert(), BorderLayout.CENTER);
+	private void createMenu() {
+		// 
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
 		
+		//
+		menu = new JMenu("Options");
+		menuBar.add(menu);
+		
+		//
+		JMenuItem openCategories = new JMenuItem("Categories");
+		
+		
+		JMenuItem openAbout = new JMenuItem("About");
+		openAbout.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				loadWindowAbout();
+			}
+		});
+		
+		JMenuItem exit = new JMenuItem("Exit");
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		menu.add(openCategories);
+		menu.addSeparator();
+		menu.add(openAbout);
+		menu.add(exit);
+	}
+
+	private void loadWindowAbout() {
+		if (current != null) {
+			remove(current);
+		}
+		current = new WindowAbout();
+		add(current, BorderLayout.CENTER);
+		revalidate();
+		repaint();
+	}
+	
+	private void loadSomething() {
+		current = new WindowAddAlert();
+		add(current, BorderLayout.CENTER);
 	}
 
 	/**
